a3130fa9b8dda1f46f0f8aca2c88162a8b03d6b1
carynJohansen <cjohansen@ucdavis.edu>
2018-01-15 12:14:03 -0800

    diagnostic plots

--- runR.sh
+++ runR.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -l
 
 #SBATCH -D /home/caryn89/Projects/maize_BSFG
-#SBATCH -o /home/caryn89/Projects/maize_BSFG/logs/R_out_%j.out
-#SBATCH -e /home/caryn89/Projects/maize_BSFG/logs/R_out_%j.out
-#SBATCH -J R-BSFG
+#SBATCH -o /home/caryn89/Projects/maize_BSFG/logs/bsfg_out_%j.out
+#SBATCH -e /home/caryn89/Projects/maize_BSFG/logs/bsfg_out_%j.out
+#SBATCH -J R-BSFG_4
 #SBATCH -t 35:00:00
-#SBATCH --mem=60000
+#SBATCH -N 1
+#SBATCH -c 24
+#SBATCH --mem=40000
 
 set -e
 set -o
--- scripts/farm_BSFG.R
+++ scripts/farm_BSFG.R
@@ -6,7 +6,8 @@ setwd("/home/caryn89/Projects/maize_BSFG")
 
 # set daily working directory
 
-rep = format(Sys.Date(), "%d-%m-%Y")
+#rep = format(Sys.Date(), "%d-%m-%Y")
+rep = "rep-04-12-2017-v2"
 folder = sprintf('analysis/rep-%s', rep)
 try(dir.create(folder))
 setwd(folder)
@@ -49,14 +50,14 @@ rownames(K_mod) = rownames(K)
 # Initialize the priors
 
 run_parameters = BSFG_control(
-  sampler = 'fast_BSFG',
-  #sampler = 'general_BSFG',
   simulation   = FALSE,
   scale_Y = TRUE, # internally rescales all the Y to have a mean value, z-score
   h2_divisions = 2,
   h2_step_size = NULL,
-  burn = 10000,
-  thin = 10
+  burn = 00,
+  thin = 10,
+  Posterior_folder = 'Posterior_v3',
+  k_init = 50
 )
 
 run_parameters
@@ -66,7 +67,7 @@ run_parameters
 
 #priors = BSFG_priors(
 #  fixed_var = list(V = 1,     nu = 3),
-  # tot_Y_var = list(V = 0.5,   nu = 3),
+#   tot_Y_var = list(V = 0.5,   nu = 3),
 #  tot_Y_var = list(V = 0.5,   nu = 5),
 #  tot_F_var = list(V = 18/20, nu = 20),
 #  delta_1   = list(shape = 2.1,  rate = 1/20),
@@ -76,7 +77,8 @@ run_parameters
 #  B_F_df    = 3,
 #  h2_priors_resids_fun = function(h2s,n) 1,#pmax(pmin(ddirichlet(c(h2s,1-sum(h2s)),rep(2,length(h2s)+1)),10),1e-10),
 #  h2_priors_factors_fun = function(h2s,n) 1#ifelse(h2s == 0,n,n/(n-1))
-#)
+#)i
+
 priors <- BSFG_priors()
 
 #----------------------
@@ -99,21 +101,31 @@ BSFG_state = BSFG_init(Y,
 #Ran it with n_samples = 100 for 1:70 loops. Put the time in as 24 hours, but this was going to take more that 24 hours. 
 # it look 16 loops about 6 hours, at a rate of 2.6 loops per hour. If you only save the BSFG_state at the end of the run, it will not complete.
 
-n_samples = 100;  # how many samples to collect at once?
-for(i  in 1:70) {
+n_samples = 500;  # how many samples to collect at once?
+for(i in 1:30) {
   print(sprintf('Run %d',i))
+  print(sprintf('n_samples',n_samples))
   BSFG_state = sample_BSFG(BSFG_state,n_samples,grainSize=1)  # run MCMC chain n_samples iterations. grainSize is a parameter for parallelization (smaller = more parallelization)
-  
+  BSFG_state = save_posterior_chunk(BSFG_state)  # save any accumulated posterior samples in the database to release memory
+  print(BSFG_state) # print status of current chain
+  plot(BSFG_state)
+
   # set of commands to run during burn-in period to help chain converge
-  if(BSFG_state$current_state$nrun < BSFG_state$run_parameters$burn - 100) {
+  #if(BSFG_state$current_state$nrun < BSFG_state$run_parameters$burn || i < 10) {
+  if(i < 20) {
     BSFG_state = reorder_factors(BSFG_state) # Factor order doesn't "mix" well in the MCMC. We can help it by manually re-ordering from biggest to smallest
-    #BSFG_state$current_state = update_k(BSFG_state) # use to drop insignificant factors
+    BSFG_state = clear_Posterior(BSFG_state)
+    # BSFG_state$current_state = update_k(BSFG_state) # use to drop insignificant factors
     BSFG_state$run_parameters$burn = max(BSFG_state$run_parameters$burn,BSFG_state$current_state$nrun+100) # if you made changes, set a new burn-in period
     print(BSFG_state$run_parameters$burn)
   }
-  BSFG_state = save_posterior_chunk(BSFG_state)  # save any accumulated posterior samples in the database to release memory
-  print(BSFG_state) # print status of current chain
-  plot(BSFG_state) # make some diagnostic plots. These are saved in a pdf booklet: diagnostic_plots.pdf
+  lambda = BSFG_state$current_state$Lambda
+  saveRDS(lambda,file = 'current_Lambda.rds')
+  boxplot(lambda)
+  dev.off()
+#  BSFG_state = save_posterior_chunk(BSFG_state)  # save any accumulated posterior samples in the database to release memory
+#  print(BSFG_state) # print status of current chain
+#  plot(BSFG_state) # make some diagnostic plots. These are saved in a pdf booklet: diagnostic_plots.pdf
   save(BSFG_state,file="current_loop.RData")
 }
 
